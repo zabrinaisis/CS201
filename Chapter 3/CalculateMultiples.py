fc2bae443cca580e5fb1e56362c192a619b66b30
zabrinaisis <zabrinaisis@gmail.com>
2015-03-03 12:49:41 -0600

    Calculate first four multiples of n.
    Asks for input in the form of an int and then provides the first four
    multiples of that int.

---
+++ Chapter 3/CalculateMultiples.py
@@ -0,0 +1,7 @@
+positive = int(input("Please input a positive integer n: "))
+
+print("Here are the first four multiples of n:")
+print(positive * 0)
+print(positive * 1)
+print(positive * 2)
+print(positive * 3)
