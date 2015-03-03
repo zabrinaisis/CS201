ef12c80803a312b118f0c2dea9491046a82cb618
zabrinaisis <zabrinaisis@gmail.com>
2015-03-03 13:00:14 -0600

    Range examples.
    Shows that different forms of range options in Python.

---
+++ Chapter 3/Ranges.py
@@ -0,0 +1,14 @@
+for i in range(11):
+	print(i)
+	
+for i in range(1,10):
+	print(i)
+	
+for i in range(0,9,2):
+	print(i)
+	
+for i in range(1,10,2):
+	print(i)
+	
+for i in range(20,61,10):
+	print(i)
